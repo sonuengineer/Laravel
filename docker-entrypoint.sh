a095c7e047456cae02b184c9b6f970696b1e53cb
Your Name <prajapatisonu50@GMAIL.com>
2025-12-26 16:06:28 +0530

    Seed admin user

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 echo "Running Laravel migrations..."
 php artisan migrate --force || true
 
-php artisan db:seed --class=AdminUserSeeder || true
+php artisan db:seed --class=UserSeeder|| true
 
 echo "Starting Apache..."
 exec "$@"
